0485a87e5568e2d9eda87d956fa60b232febe663
sonuchamlewad <csneha2620@gmail.com>
2022-06-26 23:02:18 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Sprint1';
+  public totalItem : number = 0;
+  constructor(private _auth:AuthService,private cartservice:CartService){ }
+
+  ngOnInit(): void {
+    this.cartservice.getProducts()
+    .subscribe(res=>{
+      this.totalItem = res.length;
+    })
+  }
+
+  title = 'ecommerce-web';
+
+
+  LoggedIn(input:boolean){
+    if(input){
+      return this._auth.loggedIn();
+    }
+    else{
+      return !this._auth.loggedIn();
+    }
+  }
+  LogOut(){
+    this._auth.logoutUser();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AccountComponent } from './account/account.component';
+import { HomeComponent } from './home/home.component';
+import { AuthService } from './services/auth.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AccountComponent,
+    HomeComponent,
+    DashboardComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
